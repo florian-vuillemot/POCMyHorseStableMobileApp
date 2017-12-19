2a64a8e27093238785604f320a5be0791aa1be93
florian-vuillemot <florian.vuillemot@epitech.eu>
2017-12-19 08:19:06 +0100

    First information about horse and pension. Add picture.

--- MyHorseStable/lib/Horse.dart
+++ MyHorseStable/lib/Horse.dart
@@ -38,7 +38,7 @@ class _HorseAppBarBottomSampleState extends State<_HorseAppBarBottomSample> with
     return choices.map((_HorseInfo choice) {
       return new Padding(
         padding: const EdgeInsets.all(3.0),
-        child: new _HorseInfoCard(choice: choice),
+        child: choice.widget//new _HorseInfoCard(choice: choice),
       );
     }).toList();
   }
@@ -79,38 +79,125 @@ class _HorseAppBarBottomSampleState extends State<_HorseAppBarBottomSample> with
 }
 
 class _HorseInfo {
-  const _HorseInfo({ this.title, this.icon });
+  _HorseInfo({ this.title, this.icon, this.widget });
   final String title;
   final IconData icon;
+  final Widget widget;
 }
 
-const List<_HorseInfo> choices = const <_HorseInfo>[
-  const _HorseInfo(title: 'Info cheval', icon: Icons.directions_car),
-  const _HorseInfo(title: 'Planning', icon: Icons.directions_bike),
-  const _HorseInfo(title: 'Futur rdv', icon: Icons.directions_boat),
-  const _HorseInfo(title: 'Ancien rdv', icon: Icons.directions_bus),
-  const _HorseInfo(title: 'Pension', icon: Icons.directions_railway),
+List<_HorseInfo> choices = <_HorseInfo>[
+  new _HorseInfo(title: 'Info cheval', icon: Icons.directions_car, widget: new _HorseInfoCard()),
+  new _HorseInfo(title: 'Planning', icon: Icons.directions_bike, widget: new _PlanningInfoCard()),
+  new _HorseInfo(title: 'Futur rdv', icon: Icons.directions_boat, widget: new _NextAppointmentInfoCard()),
+  new _HorseInfo(title: 'Ancien rdv', icon: Icons.directions_bus, widget: new _PreviousAppointmentInfoCard()),
+  new _HorseInfo(title: 'Pension', icon: Icons.directions_railway, widget: new _PensionInfoCard()),
 ];
 
-const List<_HorseInfo> ex = const <_HorseInfo>[
-  const _HorseInfo(title: 'Info cheval', icon: Icons.directions_car),
-  const _HorseInfo(title: 'Planning', icon: Icons.directions_bike),
-  const _HorseInfo(title: 'Futur rdv', icon: Icons.directions_boat),
-  const _HorseInfo(title: 'Ancien rdv', icon: Icons.directions_bus),
-  const _HorseInfo(title: 'Pension', icon: Icons.directions_railway),
-  const _HorseInfo(title: '', icon: Icons.directions_railway),
+List<_HorseInfo> ex = <_HorseInfo>[
+  new _HorseInfo(title: 'Info cheval', icon: Icons.directions_car),
+  new _HorseInfo(title: 'Planning', icon: Icons.directions_bike),
+  new _HorseInfo(title: 'Futur rdv', icon: Icons.directions_boat),
+  new _HorseInfo(title: 'Ancien rdv', icon: Icons.directions_bus),
+  new _HorseInfo(title: 'Pension', icon: Icons.directions_railway),
+  new _HorseInfo(title: '', icon: Icons.directions_railway),
 ];
 
 
-class _HorseInfoCard extends StatelessWidget {
-  const _HorseInfoCard({ Key key, this.choice }) : super(key: key);
 
-  final _HorseInfo choice;
+class _PensionInfoCard extends StatefulWidget {
+  @override
+  _PensionInfoCardState createState() => new _PensionInfoCardState();
+}
+
+class _PensionInfoCardState extends State<_PensionInfoCard> {
+  _PensionInfoCardState();
+
+  bool hasPaid = false;
+
+  void _handleSwitch(bool newValue) {
+    setState(() {
+      hasPaid = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> infos = [
+      new Text("Entrée le: 28/08/2016"), new Text("Pension classique au foin"),
+    ];
+
+    infos.add(
+      new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          new Text(hasPaid ? "La pension fut payé" : "La pension ne fut pas payé"),
+          new Switch(
+            value: hasPaid,
+            activeColor: Colors.green,
+            activeThumbImage: new AssetImage("images/done.png"),
+            inactiveThumbImage: new AssetImage("images/cancel.png"),
+            onChanged: _handleSwitch
+        )]
+      )
+    );
+
+    return new ListView(
+        children: infos
+    );
+  }
+}
+
+class _PreviousAppointmentInfoCard extends StatelessWidget {
+  _PreviousAppointmentInfoCard({ Key key }) : super(key: key);
+
+  final appointments = ["Rdv1", "Rdv2"];
 
   @override
   Widget build(BuildContext context) {
     return new ListView(
-          children: ex.map((e) => new ExampleWidget(e.title)).toList()
+        children: appointments.map((txt) => new Text(txt)).toList()
+    );
+  }
+}
+
+
+class _NextAppointmentInfoCard extends StatelessWidget {
+  _NextAppointmentInfoCard({ Key key }) : super(key: key);
+
+  final appointments = ["Rdv1", "Rdv2"];
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListView(
+        children: appointments.map((txt) => new Text(txt)).toList()
+    );
+  }
+}
+
+class _PlanningInfoCard extends StatelessWidget {
+  _PlanningInfoCard({ Key key }) : super(key: key);
+
+  final appointments = ["Rdv1 de 8h30", "Rdv2 de 9h40", "Cours machin"];
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListView(
+        children: appointments.map((txt) => new Text(txt)).toList()
+    );
+  }
+}
+
+class _HorseInfoCard extends StatelessWidget {
+  _HorseInfoCard({ Key key }) : super(key: key);
+
+  final choice = [
+    new ExampleWidget("Nom"), new ExampleWidget("Date de naissance")
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListView(
+          children: choice//.map((e) => new ExampleWidget(e)).toList()
         );
   }
 }
@@ -159,3 +246,50 @@ class _ExampleWidgetState extends State<ExampleWidget> {
     return this.title.isEmpty ? _makeButton() : _makeTextField();
   }
 }
+
+/*
+class ExampleWidget extends StatefulWidget {
+  ExampleWidget(this.title, {Key key}) : super(key: key);
+
+  final String title;
+
+  @override
+  _ExampleWidgetState createState() => new _ExampleWidgetState(this.title);
+}
+
+/// State for [ExampleWidget] widgets.
+class _ExampleWidgetState extends State<ExampleWidget> {
+  _ExampleWidgetState(this.title);
+
+  final String title;
+  final TextEditingController _controller = new TextEditingController();
+
+  Widget _makeButton(){
+    return new Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: new RaisedButton(
+          color: const Color(0xFF42A5F5),
+          onPressed: () {
+            print("push");
+          },
+          child: new Text('Valider'),
+        )
+    );
+  }
+
+  Widget _makeTextField(){
+    return new TextField(
+      controller: _controller,
+      decoration: new InputDecoration(
+          labelText: this.title,
+          hintText: 'Type something'
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return this.title.isEmpty ? _makeButton() : _makeTextField();
+  }
+}
+*/
